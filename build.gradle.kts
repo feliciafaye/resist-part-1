da9f8356b5ae47a6826d5503b97bec1c43448103
faye <poolsurfing@gmail.com>
2022-07-24 20:03:15 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,18 +15,18 @@ val quarkusPlatformVersion: String by project
 
 dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson:2.8.3.Final")
-    implementation("io.quarkus:quarkus-smallrye-reactive-messaging-amqp:2.8.3.Final")
+    implementation("io.quarkus:quarkus-resteasy-reactive-jackson:2.9.2.Final")
+    implementation("io.quarkus:quarkus-smallrye-reactive-messaging-amqp:2.9.2.Final")
     implementation("io.quarkus:quarkus-resteasy-reactive:2.8.1.Final")
-    implementation("io.quarkus:quarkus-smallrye-fault-tolerance:2.8.3.Final")
-    implementation("io.quarkus:quarkus-kotlin:2.8.3.Final")
+    implementation("io.quarkus:quarkus-smallrye-fault-tolerance:2.9.2.Final")
+    implementation("io.quarkus:quarkus-kotlin:2.9.2.Final")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("io.quarkus:quarkus-arc:2.8.3.Final")
-    implementation("io.smallrye.reactive:mutiny-kotlin:1.4.0")
-    implementation("io.quarkus:quarkus-smallrye-health:2.8.3.Final")
+    implementation("io.quarkus:quarkus-arc:2.9.2.Final")
+    implementation("io.smallrye.reactive:mutiny-kotlin:1.5.0")
+    implementation("io.quarkus:quarkus-smallrye-health:2.9.2.Final")
     
-    testImplementation("io.quarkus:quarkus-junit5:2.8.3.Final")
-    testImplementation("io.rest-assured:rest-assured:5.0.1")
+    testImplementation("io.quarkus:quarkus-junit5:2.9.2.Final")
+    testImplementation("io.rest-assured:rest-assured:5.1.1")
 }
 
 group = "de.faye"
